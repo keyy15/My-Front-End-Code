e51afcb02563066e3a4b019a0277a330e4faaa91
keyy15 <smeyhem01@gmail.com>
2022-06-27 12:50:13 +0700

    First Commit

---
+++ js/slide.js
@@ -0,0 +1,24 @@
+      var swiper = new Swiper(".mySwiper", {
+          slidesPerView: 1,
+          spaceBetween: 5,
+          autoplay: true,
+          loop: true,
+          pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+          },
+          breakpoints: {
+              640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+              },
+              768: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+              },
+              1024: {
+                  slidesPerView: 5,
+                  spaceBetween: 50,
+              },
+          },
+      });
